49fd476d44054960e9b196cfbb84c129e31a8d6b
Nikita <cristian.p.nikita@gmail.com>
2022-09-08 23:24:45 +0100

    ISSUE_10_rules: see description
    Rather than having a wrapper, I decided to go for an if check.
    It makes more sense like this.
    
    It makes no sense to call a function with a global. I got
    rid of the second argument.
    
    weight_rule was broken.
    
    Implemented new checks to see whether we should run
    the extra checks which are fixable.

--- json_rules.py
+++ json_rules.py
@@ -1,7 +1,25 @@
 import json
+import re
 from typing import List, Callable, Tuple, Set
 
 FILE_NAME = "sanitized.json"
+
+ALLOWED_SYMBOLS_RE = [
+        r'\+',
+        r'\-',
+        r'\"',
+        r'\:',
+        r'[a-zA-Z0-9]',
+        r'\.',
+        r'\<',
+        r'\>',
+        r'\{',
+        r'\}',
+        r'\,',
+        r'\n',
+        r'\_',
+        r' ',
+    ]
 
 def find_occurrences(string: str, character_to_find: chr) -> List[int]:
     """
@@ -20,10 +38,79 @@ def find_occurrences(string: str, character_to_find: chr) -> List[int]:
         if letter == character_to_find
     ]
 
-def message_is_readable(func: Callable) -> Tuple[bool, str]:
+
+def has_enough_lines(json_lines: List[str]) -> bool:
     """
-    A wrapper which checks whether there are at least 2 lines
-    in the message. 
+    At the very least, the message should more than 2 lines
+
+    In:
+        json_lines: The lines which make up the message
+    
+    Out:
+        True, if both braces are present in the first
+        and last line
+    """
+    return len(json_lines) > 2
+
+
+def has_braces(json_lines: List[str]) -> bool:
+    """
+    The message should have the braces.
+
+    In:
+        json_lines: The lines which make up the message
+    
+    Out:
+        True, if both braces are present in the first
+        and last line
+    """ 
+    return (
+        '{' in json_lines[0] 
+        and '}' in json_lines[-1]
+    )
+
+
+def has_most_important_fields(json_lines: List[str]) -> bool:
+    """
+    A message could have only the weight, number of repetitions and
+    exercise name and we could deduce the rest.
+
+    In:
+        json_lines: The lines which make up the message
+    
+    Out:
+        True, if the most important fields are present
+    """
+    # In older messages, I used to use "total_weight". Now I use "weight"
+    old_most_important_fields = {"exercise", "total_weight", "reps"}
+    new_most_important_fields = {"exercise", "weight", "reps"}
+    
+    for line in json_lines[1:-1]:
+        key = line.split(':')[0]
+        key = key.replace('"',"")
+        key = key.strip()
+        old_most_important_fields -= {key} 
+        new_most_important_fields -= {key}
+        if (
+            len(old_most_important_fields) == 0
+            or  len(new_most_important_fields) == 0
+        ):
+            return True
+    
+    return False
+
+
+CHECKS = [
+    has_enough_lines,
+    has_braces,
+    has_most_important_fields,
+]
+        
+
+def message_is_readable(json_lines: List[str]) -> bool:
+    """
+    A set of functions which determine whether we should even
+    bother checking the other rules
 
     In:
         func: function to be called, one of the rules
@@ -31,13 +118,11 @@ def message_is_readable(func: Callable) -> Tuple[bool, str]:
         True, "reason" if the rule specified in "func" is
         broken or the message is not readable
     """
-    def wrap(*args, **kwargs):
-        if len(args[0]) <= 2:
-            return True, "readability_rule"
-        result = func(*args, **kwargs)
-        return result
 
-    return wrap
+    return all(
+        check(json_lines) 
+        for check in CHECKS
+    )
 
 
 def split_json_message_into_lines(message: str) -> List[str]:
@@ -51,13 +136,14 @@ def split_json_message_into_lines(message: str) -> List[str]:
     """
     return message.split('\n')
 
+
 def recreate_json(json_lines_list: List[str]) -> str:
     """
     This recreates the initial JSON string
     """
     return '\n'.join(json_lines_list)
 
-@message_is_readable
+
 def breaks_braces_rule(json_lines: List[str]) -> Tuple[bool, str]:
     """
     The first and last lines of each message should be '{' and '}'.
@@ -78,7 +164,7 @@ def breaks_braces_rule(json_lines: List[str]) -> Tuple[bool, str]:
         return False, ""
     return True, "braces_rule"
 
-@message_is_readable
+
 def breaks_comma_rule(json_lines: List[str]) -> Tuple[bool, str]:
     """
     1. Only 1 comma is allowed at the end of a line
@@ -124,7 +210,7 @@ def breaks_comma_rule(json_lines: List[str]) -> Tuple[bool, str]:
     
     return False, ""
 
-@message_is_readable
+
 def breaks_colon_rule(json_lines: List[str]) -> Tuple[bool, str]:
     """
     Only one ':' should be on each line
@@ -137,14 +223,14 @@ def breaks_colon_rule(json_lines: List[str]) -> Tuple[bool, str]:
         True, "colon_rule" if the rule written above is broken.
         False, "" otherwise
     """
-    for line in json_lines[1:-2]:
+    for line in json_lines[1:-1]:
         colons = line.count(':')
         if colons != 1:
             return True, "colon_rule"
     
     return False, ""
 
-@message_is_readable
+
 def breaks_quote_rule(json_lines: List[str]) -> Tuple[bool, str]:
     """
     There should be either 2 or 4 quotation marks on each line.
@@ -156,14 +242,14 @@ def breaks_quote_rule(json_lines: List[str]) -> Tuple[bool, str]:
         True, "quote_rule" if the rule written above is broken.
         False, "" otherwise
     """
-    for line in json_lines[1:-2]:
+    for line in json_lines[1:-1]:
         quotes = line.count('"')
         if quotes not in {2, 4}:
             return True, "quote_rule"
     
     return False, ""
 
-@message_is_readable
+
 def breaks_weight_rule(json_lines: List[str]) -> Tuple[bool, str]:
     """
     Weight should be an int, a float or an expression.
@@ -179,14 +265,15 @@ def breaks_weight_rule(json_lines: List[str]) -> Tuple[bool, str]:
         False, "" otherwise
     """
     weight_line = ""
-    for line in json_lines[1:-2]:
+    for line in json_lines[1:-1]:
         if "weight" in line:
             weight_line = line
             break
     weight_value = weight_line.split(':')
+    value = weight_value[1].replace(',',"").strip()
     try:
-        float(weight_value)
-    except TypeError:
+        float(value)
+    except ValueError:
         # If we can't convert automatically 
         # to float, it's probably supposed
         # to be an expression
@@ -194,11 +281,11 @@ def breaks_weight_rule(json_lines: List[str]) -> Tuple[bool, str]:
     
     return False, ""
 
-@message_is_readable
+
 def breaks_misc_extra_symbols_rule(json_lines: List[str]) -> Tuple[bool, str]:
     """
     The only allowable symbols should be : +, -, ", :, [a-zA-Z0-9], ., <, >, {
-    }, , , \n, _
+    }, , , \n, _,  ,
 
     In:
         json_lines: The lines which make up the message
@@ -208,8 +295,19 @@ def breaks_misc_extra_symbols_rule(json_lines: List[str]) -> Tuple[bool, str]:
         False, "" otherwise
 
     """
-    #TODO : Implement
-    return False, ""
+    
+    text = recreate_json(json_lines)
+    for pattern in ALLOWED_SYMBOLS_RE:
+        if len(text) == 0:
+            break
+        text = re.sub(pattern, "", text)
+        
+    
+    if len(text) == 0:
+        return False, ""
+
+    return True, "misc_rule"
+
 
 RULES = [
     breaks_comma_rule,
@@ -220,7 +318,8 @@ RULES = [
     breaks_misc_extra_symbols_rule,
 ]
 
-def check_rules(json_lines: List[str], rules_list: List[Callable]) -> Set[str]:
+
+def check_rules(json_lines: List[str]) -> Set[str]:
     """
     Takes as input the JSON lines and a set of rules to check.
     The rules are callable functions which return tuples of type 
@@ -228,19 +327,22 @@ def check_rules(json_lines: List[str], rules_list: List[Callable]) -> Set[str]:
 
     In:
         json_lines: The lines which make up the message
-        rules_list: A list of callable functions
-    
+
     Out:
         A set of all broken rules for this message
     """
+    if not message_is_readable(json_lines):
+        return {"unreadable"}
+
     broken_rules = set()
-    for check in rules_list:
+    for check in RULES:
         broken, rule = check(json_lines)
         if broken:
             broken_rules.add(rule)
     return broken_rules
 
-
+total_broken = 0
+unreadable = 0
 
 with open(FILE_NAME, 'r') as file:
     content = json.loads(file.read())
@@ -248,15 +350,21 @@ with open(FILE_NAME, 'r') as file:
     for message in content["messages"]:
         json_message = message["text"]
         json_lines : List[str] = split_json_message_into_lines(json_message)
-        broken_rules = check_rules(json_lines, RULES)
+        broken_rules = check_rules(json_lines)
+        total_broken += len(broken_rules) > 0
+        unreadable += "unreadable" in broken_rules
         dictionary[message["id"]] = broken_rules
 
-"""
+print(total_broken)
+print(unreadable)
+
 s = set()
 for k,v in dictionary.items():
+    if 'misc_rule' in v:
+        print (dictionary[k])
+        print(k)
     if 'colon_rule' in v:
         print (dictionary[k])
     s = s.union(v)
 
 print(s)
-"""
