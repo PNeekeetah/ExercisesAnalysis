8b6f1bf956d8af4b9d75b18db1f98d42ac6710e1
Nikita <cristian.p.nikita@gmail.com>
2022-11-29 18:27:11 +0000

    Fixed

--- json_fixers.py
+++ json_fixers.py
@@ -4,9 +4,12 @@ from json_rules import content
 print(content["messages"][2773]["text"])
 
 def reparator_bulangiu_di_virguli():
+    global content
     for i, broken_rules in jr.dictionary.items():
         if ('comma_rule' in broken_rules):
             content["messages"][i]["text"]=content["messages"][i]["text"].replace(',','')
+
+reparator_bulangiu_di_virguli()
 
 print(content["messages"][2773]["text"])
 
