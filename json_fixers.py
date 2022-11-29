8be2f2d8cb6a15c0c3065e9f232fd00c3017344c
emanuelspita4 <113784211+emanuelspita4@users.noreply.github.com>
2022-11-29 19:56:57 +0200

    Add files via upload

---
+++ json_fixers.py
@@ -0,0 +1,13 @@
+import json_rules as jr
+from json_rules import content
+
+print(content["messages"][2773]["text"])
+
+def reparator_bulangiu_di_virguli():
+    for i, broken_rules in jr.dictionary.items():
+        if ('comma_rule' in broken_rules):
+            content["messages"][i]["text"].replace(',','')
+
+print(content["messages"][2773]["text"])
+
+
